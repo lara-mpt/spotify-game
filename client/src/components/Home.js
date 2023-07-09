56075e13205e538f106f1ef7889d50f5da571acf
lara_mpt <laratollington@gmail.com>
2023-07-09 14:26:54 +0100

    Clean up home component

--- client/src/components/Home.js
+++ client/src/components/Home.js
@@ -3,22 +3,24 @@ import {ArrowRightCircleFill} from 'react-bootstrap-icons';
 function Home({ roundsPlayed, score, startGame }) {
     const showStart = roundsPlayed === 0;
 
+    const startMessage = (
+        <div>
+            <h2>Welcome to Spotify Album Guesser!</h2>
+            <p>Press play to get started, and match up the album artwork to the track.</p>
+        </div>
+    );
+
+    const finishMessage = (
+        <div>
+            <h2>Congratulations, you got a score of {score}/5</h2>
+            <p>Play again?</p>
+        </div>
+    );
+
     return (
         <div>
-            {
-                showStart ? (
-                    <div>
-                        <h2>Welcome to Spotify Album Guesser!</h2>
-                        <p>Press play to get started, and match up the album artwork to the track.</p>
-                    </div>
-                ) : (
-                    <div>
-                        <h2>Congratulations, you got a score of {score}/5</h2>
-                        <p>Play again?</p>
-                    </div>
-                )
-            }
-            <ArrowRightCircleFill className={"startButton"} onClick={startGame}></ArrowRightCircleFill>
+            {showStart ? startMessage : finishMessage}
+            <ArrowRightCircleFill className={"startButton"} onClick={startGame} />
         </div>
     )
 }
